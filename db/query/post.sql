3c61f7063f924dfbc2d47547425319584e465caf
AbdullayevDilshodbek <dondilshod096@gmail.com>
2024-02-19 15:02:39 +0500

    add post sqlc query

---
+++ db/query/post.sql
@@ -0,0 +1,21 @@
+-- name: CreatePost :one
+INSERT INTO posts (title, body, user_id, status)
+VALUES ($1, $2, $3, $4)
+RETURNING *;
+
+-- name: GetPost :one
+SELECT * FROM posts
+WHERE id = $1;
+
+-- name: GetPostsList :many
+SELECT * FROM posts;
+
+-- name: UpdatePost :one
+UPDATE posts
+SET title = $2, body = $3, user_id = $4, status = $5
+where id = $1
+RETURNING *;
+
+-- name: DeletePost :exec
+DELETE from posts
+WHERE id = $1;
